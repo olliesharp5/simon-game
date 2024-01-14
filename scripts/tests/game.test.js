8db7106bd6927e4a0d8f35e82c81ce3927bc234b
Oliver Sharp <olliesharp5@gmail.com>
2024-01-14 12:56:02 +0000

    Added choices test

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -26,4 +26,7 @@ describe("game object contains correct keys", () => {
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
     });
+    test("choices contains the correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
 });
